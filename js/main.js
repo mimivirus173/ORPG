9bb56e2a557d0a3f2ae14a88cbd955dd7a4b8e69
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-07-21 13:34:01 +0100

    animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+// Main
+setTimeout(() => {
+    document.querySelector('game').style.display = "block";
+}, 4000);
+
+// stuff
 function startUpdateStats() {
 
 }
 
 function fixStuff() {
-    
+
 }
