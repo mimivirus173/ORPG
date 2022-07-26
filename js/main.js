6312995d67a1d2efd95d983e557b1856bd482ac8
mimivirus173 <87197485+mimivirus173@users.noreply.github.com>
2022-07-26 17:53:26 +0100

    oh no the button

--- js/main.js
+++ js/main.js
@@ -4,6 +4,15 @@ setTimeout(() => {
 }, 4000);
 */
 
+function switchTab(tabs) {
+    const tabs = ['crafting', 'mining', 'location', 'power', 'transport', 'stats', 'settings'];
+
+    for (const v in tabs) {
+        document.getElementById(tabs[v]).style.display = 'none';
+    }
+    document.getElementById(`${tab}`).style.display = 'block';
+}
+
 // stuff
 function startUpdateStats() {
 
